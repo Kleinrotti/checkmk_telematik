366259eca98c2b552780ad1d19b61698fcc95ad5
Kleinrotti <kleinrotti@gmail.com>
2023-05-02 22:41:44 +0200

    updated release workflow

--- .devcontainer/build.sh
+++ .devcontainer/build.sh
@@ -5,11 +5,3 @@ rm /omd/sites/cmk/var/check_mk/packages/* ||:
 ln -s $WORKSPACE/src/package /omd/sites/cmk/var/check_mk/packages/$NAME
 
 mkp -v pack $NAME
-
-# Set Outputs for GitHub Workflow steps
-if [ -n "$GITHUB_WORKSPACE" ]; then
-    echo "::set-output name=pkgfile::$(ls src/*.mkp)"
-    echo "::set-output name=pkgname::${NAME}"
-    VERSION=$(python3 -c 'print(eval(open("src/package").read())["version"])')
-    echo "::set-output name=pkgversion::$VERSION"
-fi
