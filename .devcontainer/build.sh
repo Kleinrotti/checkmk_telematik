e2653ed495b62690810d223cba5d945aef47cf1b
Kleinrotti <kleinrotti@gmail.com>
2023-05-02 21:37:41 +0200

    updated devcontainer

--- .devcontainer/build.sh
+++ .devcontainer/build.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-NAME=$(python -c 'print(eval(open("package").read())["name"])')
+NAME=$(python -c 'print(eval(open("src/package").read())["name"])')
 rm /omd/sites/cmk/var/check_mk/packages/* ||:
-ln -s $WORKSPACE/package /omd/sites/cmk/var/check_mk/packages/$NAME
+ln -s $WORKSPACE/src/package /omd/sites/cmk/var/check_mk/packages/$NAME
 
 mkp -v pack $NAME
 
@@ -10,6 +10,6 @@ mkp -v pack $NAME
 if [ -n "$GITHUB_WORKSPACE" ]; then
     echo "::set-output name=pkgfile::$(ls *.mkp)"
     echo "::set-output name=pkgname::${NAME}"
-    VERSION=$(python -c 'print(eval(open("package").read())["version"])')
+    VERSION=$(python -c 'print(eval(open("src/package").read())["version"])')
     echo "::set-output name=pkgversion::$VERSION"
 fi
