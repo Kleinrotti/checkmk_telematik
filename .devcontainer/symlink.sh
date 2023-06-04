75d8628bb0ec21f39606c79c0cc6095012506aa2
Kleinrotti <kleinrotti@gmail.com>
2023-06-04 21:16:10 +0200

    add execute permissions after container creation

--- .devcontainer/symlink.sh
+++ .devcontainer/symlink.sh
@@ -9,3 +9,5 @@ rm -rfv $OMD_ROOT/local/lib/check_mk/base/plugins/agent_based
 ln -sv $WORKSPACE/src/agent_based $OMD_ROOT/local/lib/check_mk/base/plugins/agent_based
 
 htpasswd -b $OMD_ROOT/etc/htpasswd cmkadmin cmkadmin
+
+chmod +x $OMD_ROOT/local/share/check_mk/agents/special/*
