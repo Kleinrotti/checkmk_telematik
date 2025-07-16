b376ccc82ce45c2d40bb148297939fcfe43b72d8
Kleinrotti <39203164+Kleinrotti@users.noreply.github.com>
2025-07-16 20:30:07 +0200

    updating build

--- .devcontainer/build.sh
+++ .devcontainer/build.sh
@@ -5,4 +5,4 @@ rm /omd/sites/cmk/var/check_mk/packages_local/* ||:
 ln -s $WORKSPACE/package /omd/sites/cmk/tmp/check_mk/$NAME
 
 mkp -v package /omd/sites/cmk/tmp/check_mk/$NAME
-cp /omd/sites/cmk/var/check_mk/packages_local/$NAME*.mkp ./build
+cp /omd/sites/cmk/var/check_mk/packages_local/$NAME*.mkp .
